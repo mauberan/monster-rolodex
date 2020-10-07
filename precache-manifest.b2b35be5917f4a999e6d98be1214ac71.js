d4cd6d1db7457598324480955c1cda37db67b641
Ran <ranmauber8@gmail.com>
2020-10-07 15:06:17 +0300

    Updates

---
+++ precache-manifest.b2b35be5917f4a999e6d98be1214ac71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f8503d3c934448ac67906879fb3355",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2c7ea1299d990405f2ec",
+    "url": "/monster-rolodex/static/css/main.fe7ae281.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7ea1299d990405f2ec",
+    "url": "/monster-rolodex/static/js/main.abd8707a.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
